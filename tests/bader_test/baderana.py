95098f3292584f8ed666c6c84226b3ed2717e6bb
aa <qai222@uky.edu>
2019-10-09 17:41:18 -0400

    edit bader

--- tests/bader_test/baderana.py
+++ tests/bader_test/baderana.py
@@ -26,31 +26,37 @@ One finally note is that you need a fine fft grid to accurately reproduce the co
 It is essential to do a few calculations, increasing NG(X,Y,Z)F until the total charge is correct. 
 """
 
-# ba = BaderAnalysis('CHGCAR', 'POTCAR')
+ba = BaderAnalysis('CHGCAR', 'POTCAR')  # this is a good example for running code and capturing stdout
 # print(ba.get_charge(1))
 
 from ocelot.routines.pbc import PBCparser
 from pymatgen.core.structure import Structure
+
 struct = Structure.from_file('CONTCAR')
 mols, unwrap_str_sorted, unwrap_pblock_list = PBCparser.unwrap(struct)
 
-acfdata = []
-with open('ACF.dat', 'r') as f:
-    ls = f.readlines()
-for l in ls[2:-4]:
-    iatom, x, y, z, charge, mindist, atomvol = l.strip().split()
-    entry = {}
-    entry['iatom'] = int(iatom)-1
-    entry['x'] = x
-    entry['y'] = y
-    entry['z'] = z
-    entry['charge'] = charge
-    entry['mindist'] = mindist
-    entry['atomvol'] = atomvol
-    for k in entry.keys():
-        entry[k] = float(entry[k])
-    acfdata.append(entry)
 
+def parse_acf(acfdata_fn='./ACF.dat'):
+    acfdata = []
+    with open(acfdata_fn, 'r') as f:
+        ls = f.readlines()
+    for l in ls[2:-4]:
+        iatom, x, y, z, charge, mindist, atomvol = l.strip().split()
+        entry = {}
+        entry['iatom'] = int(iatom) - 1
+        entry['x'] = x
+        entry['y'] = y
+        entry['z'] = z
+        entry['charge'] = charge
+        entry['mindist'] = mindist
+        entry['atomvol'] = atomvol
+        for k in entry.keys():
+            entry[k] = float(entry[k])
+        acfdata.append(entry)
+    return acfdata
+
+
+# acfdata = parse_acf()
 imols = [s.properties['imol'] for s in unwrap_str_sorted.sites]
 isites = [s.properties['isite'] for s in unwrap_str_sorted.sites]
 istie2imol_dict = dict(zip(isites, imols))
@@ -58,13 +64,6 @@ for imol in range(len(mols)):
     molchg = 0
     for isite in range(len(isites)):
         if istie2imol_dict[isite] == imol:
-            molchg += acfdata[isite]['charge']
+            # molchg += acfdata[isite]['charge']
+            molchg += ba.get_charge(isite)
     print(molchg)
-
-
-
-
-
-
-
-
